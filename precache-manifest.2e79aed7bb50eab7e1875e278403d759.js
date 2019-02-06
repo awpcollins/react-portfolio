3f014fa80c7c34e6c83b0955e9dcaf0dc1d0618d
Alexander Collins <awpcollins@gmail.com>
2019-02-06 23:00:07 +0000

    Updates

---
+++ precache-manifest.2e79aed7bb50eab7e1875e278403d759.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "171e19000fd1a61ea809",
+    "url": "/react-portfolio/static/js/main.171e1900.chunk.js"
+  },
+  {
+    "revision": "960df1b3ba2ead92cf1a",
+    "url": "/react-portfolio/static/js/1.960df1b3.chunk.js"
+  },
+  {
+    "revision": "171e19000fd1a61ea809",
+    "url": "/react-portfolio/static/css/main.dbf8cb78.chunk.css"
+  },
+  {
+    "revision": "29470f4b7a3864e0d613bd5e88142ec8",
+    "url": "/react-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/awpcollins/react-portfolio/precache-manifest.d49785e3a7ee98897d993149384f294e.js"
+  "/react-portfolio/precache-manifest.2e79aed7bb50eab7e1875e278403d759.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/awpcollins/react-portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/react-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
