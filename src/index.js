6e870ef660ab48948dc11eaae11a1413efe15441
Alexander Collins <awpcollins@gmail.com>
2019-02-06 22:12:15 +0000

    Trying to get gh pages to work

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import App from './components/App.jsx'
 import './css/custom.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+import ghpages from 'gh-pages'
+ 
+ghpages.publish('dist', function(err) {})
